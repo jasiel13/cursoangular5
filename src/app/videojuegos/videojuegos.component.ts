d4bc2f98327b5dbfed6cc37143b39165dac3dde4
jasiel13 <jasielutt@gmail.com>
2019-09-17 13:21:46 -0500

    parte intermieda y fianl del curso

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -2,9 +2,29 @@ import {Component} from "@angular/core";
 
 @Component({
     selector:'videojuegos',
-    template:
-    `<h2>Component de {{nombre}} </h2>`
+    templateUrl:'./videojuegos.component.html'
     })
     export class VideojuegosComponent{
-        public nombre ='Videojuegos 2018';
+        public nombre :string;
+        public mejor_juego : string ;
+        public mejor_juego_retro: string ;
+        public mostrar_retro:boolean;
+        public color:string;
+        public year:number;
+        public videojuegos:Array<string>;
+
+        constructor(){
+            this.nombre ='Videojuegos 2018';
+            this.mejor_juego ='GTA5';
+            this.mejor_juego_retro = 'super mario 64';
+            this.mostrar_retro=true;
+            this.color="yellow";
+            this.videojuegos=[
+                'los simpson hit and run',
+                'halo',
+                'call of duty',
+                'fifa',
+                'resident evil'
+            ];
+        }        
     }
